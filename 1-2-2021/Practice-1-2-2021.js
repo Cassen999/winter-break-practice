7625a85d7207ff920ce68e72ca0896bdcac06340
Cassen Gerber <cassenpt@gmail.com>
2021-01-02 13:01:47 -0600

    did two codewars problems

---
+++ 1-2-2021/Practice-1-2-2021.js
@@ -0,0 +1,24 @@
+function numberToString(num) {
+    return num.toString()
+}
+
+console.log(numberToString(999))
+
+
+
+function filter_list(l) {
+    newArray = []
+    for (let i = 0; i < l.length; i++) {
+        if (l[i] >= 0 && Number.isFinite(l[i]) === true){
+        newArray.push(l[i])
+        }
+    }
+    return newArray
+}
+
+console.log(filter_list([1,2,'aasf','1','123',123]))
+// Output '[1, 2, 123]'
+console.log(filter_list([1,'a','b',0,15]))
+// Output '[1, 0, 15]'
+console.log(filter_list([1,2,'a','b']))
+// Output '[1, 2]'
---
+++ 1-2-2021/README.md
@@ -0,0 +1,5 @@
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
